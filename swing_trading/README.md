82669e6e7ca116baa578d64091ee6fa429712838
ikki02 <B1200315@gl.aiu.ac.jp>
2025-12-24 17:43:37 +0900

    refactor: swing_trading

--- swing_trading/README.md
+++ swing_trading/README.md
@@ -1,5 +1,7 @@
 # 環境構築
-uv sync
+`% uv venv`  
+`% source .venv/bin/activate`  
+`% uv sync`  
 
 # Pythonファイル説明
 CheckTechnicalBlocker.py
@@ -14,9 +16,9 @@ VolumeSpike.py
 - 使い方: slackの環境変数設定後、uv run VolumeSpike.py
 
 get_kabutan.py
-- 市場(東証)と出来高で絞り込まれた、PTS市場の値上がり銘柄の一覧を取得する
+- 市場(東証)と出来高で絞り込まれた、出来高急上昇銘柄の一覧を取得する
 - ローカルにcsv出力する
-- 使い方: 使いたい関数だけコメントアウトを外して、uv run get_kabutan.py
+- 使い方: `% uv run get_kabutan.py`
 
 # ToDo
 - 株探のassertつけたい
--- swing_trading/get_kabutan.py
+++ swing_trading/get_kabutan.py
@@ -2,81 +2,6 @@ from typing import Iterable
 import pandas as pd
 
 
-def get_kabutan_pts_stocks(
-    url: str = "https://kabutan.jp/warning/pts_night_price_increase",
-    markets: Iterable[str] | None = ["東Ｐ", "東Ｓ", "東Ｇ", "東Ｅ"],
-    volume: int = 100_000,
-) -> pd.DataFrame:
-    """
-    株探（kabutan）の探索ページから銘柄一覧を取得する
-
-    Args:
-        url: PTS市場の株価上昇率ランキングや出来高ランキングのURL
-        markets: 許可する市場コード（例: ["東Ｐ", "東Ｓ", "東Ｇ", "東Ｅ"]）。None の場合はフィルタしない
-        volume: 出来高の下限
-
-    Returns:
-        市場と出来高で絞り込まれた、PTS市場の値上がり銘柄の一覧
-    """
-    # HTML内のランキング50件を取得する。for文でまわさない場合15件しか取れないため、pageパラメータを回して上位50件を取得する
-    dfs = []
-    for page in range(1, 3):  # 1〜4 くらいまで試す
-        url = f"{url}?page={page}"
-        table = pd.read_html(url)[2]  # ページ構造によってインデックス調整。# [2]に今回欲しいランキングがある。
-        dfs.append(table)
-    df = pd.concat(dfs, ignore_index=True)
-
-    # 列が銘柄のマルチインデックスのため、この2段重ねのうち、列名（1番目）だけ残したい
-    df.columns = df.columns.get_level_values(1)
-
-    # Unnamed 列を削除
-    df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
-
-    # カラム名を正規化
-    df = df.rename(
-        columns={
-            "通常取引 23日終値": "株価",
-            "株価": "PTS株価",
-            "通常取引 23日終値比": "通常取引23日終値比(実数)",
-            "通常取引 23日終値比.1": "通常取引23日終値比(率)",
-            "ＰＥＲ": "PER",
-            "ＰＢＲ": "PBR",
-        }
-    )
-
-    for col in ["PER", "PBR", "利回り"]:
-        df[col] = pd.to_numeric(df[col], errors="coerce")
-
-    # 必要な列だけ残す（列順も固定）
-    df = df[
-        [
-            "銘柄名",
-            "コード",
-            "市場",
-            "株価",
-            "PTS株価",
-            "通常取引23日終値比(実数)",
-            "通常取引23日終値比(率)",
-            "出来高",
-            "PER",
-            "PBR",
-            "利回り",
-        ]
-    ]
-
-    # 市場で絞る
-    if markets is not None:
-        df = df[df["市場"].isin(markets)]
-
-    # 出来高で絞る
-    df = df[df["出来高"] >= volume]
-
-    # PTS株価がプラスのものだけに絞る。
-    df = df[df["通常取引23日終値比(実数)"] > 0]
-
-    return df
-
-
 # 15分遅れ
 def get_kabutan_stocks(
     url: str = "https://kabutan.jp/tansaku/?mode=2_0311&dispmode=normal",
@@ -130,11 +55,7 @@ def get_kabutan_stocks(
 
 
 if __name__ == "__main__":
-    df = get_kabutan_pts_stocks(
-        url="https://kabutan.jp/warning/pts_night_price_increase", markets=["東Ｐ", "東Ｓ", "東Ｇ"]
-    )
-    # df = get_kabutan_pts_stocks(url="https://kabutan.jp/warning/pts_night_volume_ranking")
-    # df = get_kabutan_stocks()
+    df = get_kabutan_stocks()
     print(
         df.to_string(
             index=False,
@@ -142,4 +63,4 @@ if __name__ == "__main__":
             max_cols=None,
         )
     )
-    df.to_csv("kabutan_ranking.csv", index=False)
+    df.to_csv("logs/kabutan_ranking.csv", index=False)
